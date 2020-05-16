6a882cfaf3a10de3595bfa5e7744393f4b8f08b4
havocked-fury <raiantasfin@tutanota.com>
2020-05-16 16:40:46 +0600

    Project tested locally

---
+++ build.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+const char newline = '\n';
+
+std::fstream buildList, buildScript;
+std::string fileName;
+
+int main(){
+	std::ios_base::sync_with_stdio(0);
+
+	/*initBuildScript*/{
+		system("rm buildScript");
+		system("touch buildScript");
+	}
+
+	/*openBuildScript*/{
+		buildScript.open("./buildScript");
+
+		/*script write: compile files*/{
+			buildList.open("./buildList");
+
+			while(buildList>> fileName){
+				buildScript<< "g++ -c ../src/" << fileName << ".cpp" << newline;
+			}
+
+			buildList.close(); // "./buildList"
+		} 
+
+		buildList.open("./buildList");
+
+		/*script write: link files*/{
+			buildScript<< "g++ -o run";
+			while(buildList>> fileName){
+				buildScript<< " ./" <<  fileName << ".o ";
+			}
+			buildScript<<newline;
+		}
+		/*
+		/*script write: organise files*/{
+
+			buildScript<< "mv *.o ../obj" << newline;
+			buildScript<< "mv run ../ " << newline;
+		}
+
+		buildList.close(); // buildList
+
+		buildScript.close(); // "./buildScript"
+
+		/*build project*/{
+
+			system("bash ./buildScript");
+		}
+	}
+	
+	return 0;
+}
